c6cd6bff08400509373f4aacf1d6546831d6d939
Kamar <kahj23@student.bth.se>
2025-08-11 18:08:50 +0200

    Add Deck class with shuffle functionality

---
+++ src/Game/Deck.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Game;
+
+class Deck
+{
+    /** @var Card[] */
+    private array $cards = [];
+
+    public function __construct()
+    {
+        $suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+        $ranks = [
+            '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7,
+            '8' => 8, '9' => 9, '10' => 10,
+            'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 1
+        ];
+
+        foreach ($suits as $suit) {
+            foreach ($ranks as $rank => $value) {
+                $this->cards[] = new Card($suit, $rank, $value);
+            }
+        }
+    }
+
+    public function shuffle(): void
+    {
+        shuffle($this->cards);
+    }
+
+    public function draw(): ?Card
+    {
+        return array_shift($this->cards);
+    }
+
+    public function remaining(): int
+    {
+        return count($this->cards);
+    }
+}
