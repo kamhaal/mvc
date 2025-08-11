59b007bc0f81ae52e4665c1dbbfcffab8e4faaeb
Kamar <kahj23@student.bth.se>
2025-08-11 18:08:50 +0200

    Implement Hand class with value calculation

---
+++ src/Game/Hand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Game;
+
+class Hand
+{
+    /** @var Card[] */
+    private array $cards = [];
+
+    public function add(Card $card): void
+    {
+        $this->cards[] = $card;
+    }
+
+    public function getCards(): array
+    {
+        return $this->cards;
+    }
+
+    public function getValue(): int
+    {
+        $total = 0;
+        $aces = 0;
+
+        foreach ($this->cards as $card) {
+            $total += $card->getValue();
+            if ($card->getRank() === 'A') {
+                $aces++;
+            }
+        }
+
+        while ($aces > 0 && $total + 13 <= 21) {
+            $total += 13;
+            $aces--;
+        }
+
+        return $total;
+    }
+
+    public function getAsString(): string
+    {
+        $cards = [];
+        foreach ($this->cards as $card) {
+            $cards[] = $card->getAsString();
+        }
+        return implode(' ', $cards);
+    }
+}
