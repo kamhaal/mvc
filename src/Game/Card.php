be6883444cdb2d13e10ce4eda7fa8e0c19a61454
Kamar <kahj23@student.bth.se>
2025-08-11 18:08:40 +0200

    Add Card class implementation

---
+++ src/Game/Card.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Game;
+
+class Card
+{
+    private string $suit;
+    private string $rank;
+    private int $value;
+
+    public function __construct(string $suit, string $rank, int $value)
+    {
+        $this->suit = $suit;
+        $this->rank = $rank;
+        $this->value = $value;
+    }
+
+    public function getSuit(): string
+    {
+        return $this->suit;
+    }
+
+    public function getRank(): string
+    {
+        return $this->rank;
+    }
+
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return "{$this->rank} of {$this->suit}";
+    }
+
+    // Card.php
+    public function getAsString(): string
+    {
+        $symbols = [
+            'Hearts' => '♥',
+            'Diamonds' => '♦',
+            'Clubs' => '♣',
+            'Spades' => '♠'
+        ];
+
+        return $this->rank . $symbols[$this->suit];
+    }
+}
