68559c6c82c28f065941cf7c96b950296e13249e
Kamar <kahj23@student.bth.se>
2025-09-02 05:51:38 +0200

    Library

--- src/Library/Book.php
+++ src/Library/Book.php
@@ -5,7 +5,7 @@ namespace App\Library;
 use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ORM\Library(repositoryClass: BookRepository::class)]
 class Book
 {
     #[ORM\Id]
